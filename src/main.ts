a7526f3af71b10434f27c82b64a25c8c0f7caedd
MelihCanitez <melih.canitez@stud.hs-bochum.de>
2023-07-12 11:13:16 +0200

    cave and pirateship character, main.ts edited

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,11 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    WA.room.area.onEnter('start').subscribe(() => {
+        console.log("Test")
     })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('start').subscribe(closePopup)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
